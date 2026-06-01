36d669ce17c900b613ed582b0645d98a3a6926da
mfire <mfire@sina.cn>
2026-06-01 15:55:07 +0800

    精简：只保留aio-tools相关脚本

--- common/license_uuid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IP=$1
-hostid=$(hostid)
-hostidmd5=$(echo -n "${hostid}" | openssl md5 | awk -F '=' '{print $2}' | sed 's/[[:space:]]//g')
-hostipmd5=$(echo -n "${IP}" | openssl md5 | awk -F '=' '{print $2}' | sed 's/[[:space:]]//g')
-  # shellcheck disable=SC2116
-appid=$(echo "${hostidmd5}":"${hostipmd5}")
-echo "${appid}"
